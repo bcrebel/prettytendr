a22d048e36f9cb90352de157c901cb174c276b2a
Carron <carronwhite@gmail.com>
2024-08-13 17:02:14 -0500

    testing dist

---
+++ packages/soroban_increment_contract/dist/index.d.ts
@@ -0,0 +1,58 @@
+import { AssembledTransaction, Client as ContractClient, ClientOptions as ContractClientOptions } from '@stellar/stellar-sdk/contract';
+import type { u32 } from '@stellar/stellar-sdk/contract';
+export * from '@stellar/stellar-sdk';
+export * as contract from '@stellar/stellar-sdk/contract';
+export * as rpc from '@stellar/stellar-sdk/rpc';
+export declare const networks: {
+    readonly testnet: {
+        readonly networkPassphrase: "Test SDF Network ; September 2015";
+        readonly contractId: "CCDMKJP7X7KFGZPXT4YVUXRMMUBBHE5F2HGLIYIN7C2R3GH5TGEPWBMD";
+    };
+};
+export declare const Errors: {};
+export interface Client {
+    /**
+     * Construct and simulate a increment transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+     * Increment an internal counter; return the new value.
+     */
+    increment: (options?: {
+        /**
+         * The fee to pay for the transaction. Default: BASE_FEE
+         */
+        fee?: number;
+        /**
+         * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+         */
+        timeoutInSeconds?: number;
+        /**
+         * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+         */
+        simulate?: boolean;
+    }) => Promise<AssembledTransaction<u32>>;
+    /**
+     * Construct and simulate a get_value transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+     * Get the current value of the counter.
+     */
+    get_value: (options?: {
+        /**
+         * The fee to pay for the transaction. Default: BASE_FEE
+         */
+        fee?: number;
+        /**
+         * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+         */
+        timeoutInSeconds?: number;
+        /**
+         * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+         */
+        simulate?: boolean;
+    }) => Promise<AssembledTransaction<u32>>;
+}
+export declare class Client extends ContractClient {
+    readonly options: ContractClientOptions;
+    constructor(options: ContractClientOptions);
+    readonly fromJSON: {
+        increment: (json: string) => AssembledTransaction<number>;
+        get_value: (json: string) => AssembledTransaction<number>;
+    };
+}
---
+++ packages/soroban_increment_contract/dist/index.js
@@ -0,0 +1,28 @@
+import { Buffer } from "buffer";
+import { Client as ContractClient, Spec as ContractSpec, } from '@stellar/stellar-sdk/contract';
+export * from '@stellar/stellar-sdk';
+export * as contract from '@stellar/stellar-sdk/contract';
+export * as rpc from '@stellar/stellar-sdk/rpc';
+if (typeof window !== 'undefined') {
+    //@ts-ignore Buffer exists
+    window.Buffer = window.Buffer || Buffer;
+}
+export const networks = {
+    testnet: {
+        networkPassphrase: "Test SDF Network ; September 2015",
+        contractId: "CCDMKJP7X7KFGZPXT4YVUXRMMUBBHE5F2HGLIYIN7C2R3GH5TGEPWBMD",
+    }
+};
+export const Errors = {};
+export class Client extends ContractClient {
+    options;
+    constructor(options) {
+        super(new ContractSpec(["AAAAAAAAADRJbmNyZW1lbnQgYW4gaW50ZXJuYWwgY291bnRlcjsgcmV0dXJuIHRoZSBuZXcgdmFsdWUuAAAACWluY3JlbWVudAAAAAAAAAAAAAABAAAABA==",
+            "AAAAAAAAACVHZXQgdGhlIGN1cnJlbnQgdmFsdWUgb2YgdGhlIGNvdW50ZXIuAAAAAAAACWdldF92YWx1ZQAAAAAAAAAAAAABAAAABA=="]), options);
+        this.options = options;
+    }
+    fromJSON = {
+        increment: (this.txFromJSON),
+        get_value: (this.txFromJSON)
+    };
+}
