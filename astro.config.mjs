3aaf11a285a221cc498f1a18d4627375a86f4ff0
Carron <carronwhite@gmail.com>
2024-08-18 23:21:13 -0500

    node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bcrebel.github.io',
   base: 'prettytendr',
   integrations: [react(), tailwind()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
